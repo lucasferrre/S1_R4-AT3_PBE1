2caf4826c4ad8ce44c5147a450a572be0ca4aa8a
lucasferrre <lucasferreirasasuke@gmail.com>
2025-10-16 11:21:43 -0300

    finalizando atividade3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 8081;
 // nesse caso eu quero que o express entenda json, ent eu coloco esse midleware para valer para todas as rotas
 app.use(express.json());
 
-// função para validar as informações de nome e notas no array
+// função para validar as informações dos números 
 async function validaNumeros(pNumeros) {
 
     // aqui eu valido se não é um array e se a estring está vazia
